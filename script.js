408bca0490ce7d29698e383666f04b400ca555c6
Vano540 <vano.op@mail.ru>
2023-05-15 22:52:58 +0200

    new commit

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ let counter = 3;
 
 function like() {
     counter = counter + 1;
-    document.getElementById("votes1").innerHTML = counter + " votes";
+    document.getElementById("votes1").innerHTML = counter + " likes";
 }
 
 function deletelike() {
     counter = counter - 1;
-    document.getElementById("votes1").innerHTML = counter + " votes";
+    document.getElementById("votes1").innerHTML = counter + " likes";
 }
 
 let number = 3;
 
 function dislike() {
     number = number + 1;
-    document.getElementById("votes2").innerHTML = number + " votes";
+    document.getElementById("votes2").innerHTML = number + " dislikes";
 }
 
 function deletedis() {
     number = number - 1;
-    document.getElementById("votes2").innerHTML = number + " votes";
+    document.getElementById("votes2").innerHTML = number + " dislikes";
 }
